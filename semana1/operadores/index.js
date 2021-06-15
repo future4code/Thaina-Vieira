3bc37e3a5394bf4001b84b0867a1d4676077c695
Thainá <thainavieira660@gmail.com>
2021-06-15 16:56:34 -0300

    adicionei a pasta semana1 e operadores

---
+++ semana1/operadores/index.js
@@ -0,0 +1,121 @@
+/*Exercício de interpretação
+
+questao-1
+
+const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado) resposta: a.false
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) resposta: b.false
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado) resposta: c.true
+
+console.log("d. ", typeof resultado) resposta: d.boolean  */ 
+
+
+/*questao-2
+
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)
+
+resposta: Não se usa prompt para somar números. Nada foi impresso no console. */
+
+/*questao-3
+
+const primeiroValor = digite um numero
+const segundoValor = digite outro numero
+const resultado = primeiroValor + segundoValor
+
+console.log(resultado)*/
+
+
+/*Exercício de escrita de codigo
+
+questão-1
+
+letra a)
+
+
+const idade = prompt("qual a sua idade?")
+
+console.log(resultado)*/
+
+
+
+/*letra b)
+
+
+const idadeMelhoramiga = prompt("qual é a sua idade? Larissa")
+
+console.log(idadeMelhoramiga)*/
+
+
+/*letra c) false
+
+const minhaIdade = 23
+const idadeMelhoramiga = 24
+ 
+const maior = minhaIdade> idadeMelhoramiga
+
+console.log("a minhaidade é maior que a idadeMelhoramiga?", maior)*/
+
+
+/*letra d) 
+
+const minhaIdade = 23
+const idadeMelhoramiga = 24
+
+const resultado = minhaIdade - idadeMelhoramiga
+
+console.log(resultado) -1 */
+
+
+/*questão-2
+
+letra a)
+
+const coloqueUmnumeroPar = prompt("insira um número par")
+
+console.log(numeropar)*/
+
+ 
+
+/*letra b)
+
+const restoDadivisao = 2 % 2
+
+console.log(restoDadivisao) //0  */
+
+
+/*letra c)
+
+Notei que o resto é sempre um número inteiro */
+
+/*letra d)
+
+Acontece que o resto da divisão é inteiro também, como no resto da divisão dos números pares */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
