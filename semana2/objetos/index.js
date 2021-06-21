5ff7130c07a83b9f464b0cf143a470f19d0eb67c
Thainá <thainavieira660@gmail.com>
2021-06-21 16:48:57 -0300

    adicionei a pasta objetos

---
+++ semana2/objetos/index.js
@@ -0,0 +1,93 @@
+// Exercícios de interpretação de código
+
+// questao 1)
+
+// Matheus Nachtergaele
+// Vírginia Cavendich
+// (Canal: "Globo", horário: "14h")
+
+// questão 2)
+
+// a)
+// (nome: "juca", idade: 3, raca: "SRD")
+// (nome: "juba", idade: 3, raca: "SRD")
+// (nome: "jubo", idade: 3, raca: "SRD")
+
+// b)
+// Realiza uma cópia inteira de um objeto para o outro e muda/adiciona apenas uma ou outra propriedade.
+
+// questão 3)
+
+// a)
+// False
+// Undefined
+
+// b) Acho que isso aconteceu por que "altura" não está definido dentro da função.
+
+
+// Exercícios de escrita de código
+
+// questão 1)
+
+// a)
+
+ //const pessoa = {
+    //nome: "Thaina",
+    //apelidos: ["Thai", "Thatá", "Ná"]
+//}
+//function meusApelidos(){
+    //const nome = "Thainá"
+    //const apelidos = ["Thai", "Thata", "Ná"]
+    //console.log(`Eu sou ${nome}, mas pode me chamar de: ${apelidos}`)
+//}
+//meusApelidos()
+
+// b) 
+
+//const novosApelidos = (...pessoa, apelidos: ["Thainaaa", "Thainazinha", "Thainá Rodrigues"])
+//console.log(novosApelidos)
+//console.log(pessoas)
+
+// questão 2)
+
+//const dadosPessoais1 = {
+    //nome: "Maria",
+    //idade: 25,
+    //profissao: "Advogada"
+//}
+//const dadosPessoais2 = {
+    //nome: "João",
+    //idade: 32,
+    //profissao: "Professor"
+//}
+
+//const dadosPessoaisfinal = (dadosPessoais1, dadosPessoais2) => {
+    //const dadosFinais = {dadosPessoais1.nome, dadospessoais.nome.length, dadosPessoais1.idade, dadosPessoais1.profissao, dadosPessoais1.profissao.length, dadosPessoais2.nome, dadosPessoais2.nome.lenght, dadosPessoais2.idade, dadosPessoais.profissao, dadosPessoais2.profissao.length}
+
+    //return dadosFinais
+//}
+
+//console.log(dadosPessoaisfinal(dadosPessoais1, dadosPessoais2))
+
+// questao 3)
+
+
+//let carrinho =[]
+
+//const fruta1 = {
+    //nome : "maça",
+    //disponibilidade : true
+//}
+//const fruta2 = {
+    //nome : "pitaia",
+    //disponibilidade : true
+//}
+//const fruta3 = {
+    //nome : "banana",
+    //disponibilidade : true
+//}
+    //function sacola(fruta1, fruta2, fruta3){
+        //carrinho.push(fruta1, fruta2, fruta3)
+    //}
+    //sacola(fruta1, fruta2, fruta3)
+    //console.log(carrinho)
