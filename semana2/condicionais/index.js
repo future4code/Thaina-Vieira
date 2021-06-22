671295be91bc3bb65911f566dfce4a5b089aa2e2
Thainá <thainavieira660@gmail.com>
2021-06-22 16:43:33 -0300

    adicionei a nova pasta condicional

---
+++ semana2/condicionais/index.js
@@ -0,0 +1,82 @@
+// Eexercícios de interpretação de codigos
+
+// questao 1)
+
+// a) Ele testa o resto da divisão
+// b) Para números pares
+// c) Para números ímpares
+
+// questão 2)
+
+// a) Para dizer o valor da fruta escolhida
+// b) O preço da fruta maçã é R$5
+// c) O preço da fruta pÊra é R$5
+
+// questão 3)
+
+// a) Pedindo para o usuario digitar um número
+// b) Esse número passou no teste. Se digitar -10 aparece "mensagem is not defined"
+// c) Apareceu um erro "mensagem is not defined", por que o -10 não foi declarado no escopo local.
+
+// Exercícios de escrita de código
+
+// questão 1)
+
+//const idadeUsuario = Number(prompt("Digite sua idade"))
+//const checaMaioridade = idadeUsuario >= 18 
+
+//const podeOuNaodirigir = (checaMaioridade) => {
+    //if(checaMaioridade){
+        //console.log("Voce pode dirigir")
+    //}else{
+        //console.log("voce nao pode dirigir")
+    //}
+//} 
+//podeOuNaodirigir(checaMaioridade)
+
+// questão 2)
+
+//const turno = prompt("digite o seu turno: M, V ou N")
+//const comparaTurno = (M, V, N) => {
+    //if(turno === M) {
+        //console.log("bom dia")
+    //} else if (turno === V) {
+        //console.log("boa tarde")
+    //} else if (turno === N) {
+        //console.log("boa noite")
+    //}
+//}
+//comparaTurno(turno)
+
+// questão 3)
+
+//const turno = prompt("digite o seu turno: M, V ou N")
+//switch(turno){
+  //case "M":
+    //console.log("bom dia")
+    //break
+  //case "V":
+      //console.log("boa tarde")
+      //break  
+   //case "N":
+       //console.log("boa noite")
+       //break
+    //}
+
+    // questão 4)
+
+    //const generoFilme = prompt("qual o genero do filme que vai assistir?")
+    //const precoFilme = Number(prompt("qual o valor do ingresso?"))
+
+    //const filme = (generoFilme, precoFilme)=> {
+        //if (generoFilme === "fantasia"){
+            //console.log("bom filme")
+        //}else if (precoFilme <= 15){
+            //console.log("bom filme")
+        //}else if (generoFilme !== "fantasia"){
+            //console.log("escolha outro filme")
+        //}else if (precoFilme > 15){
+            //console.log("escolha outro filme")
+        //}
+    //}
+//filme(generoFilme, precoFilme)
