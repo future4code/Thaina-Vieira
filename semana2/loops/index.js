b85b28ebacdb2d4fc430a728883283dde7743a80
Thainá <thainavieira660@gmail.com>
2021-06-23 16:27:23 -0300

    adicionei a pasta loos

---
+++ semana2/loops/index.js
@@ -0,0 +1,36 @@
+// Exercícios de interpretação de códigos
+ 
+// questão 1)
+
+// Será impresso no console: 10
+
+// questão 2)
+
+// a) 19, 21, 23, 25, 27, 30
+
+// b)
+
+// questão 3)
+
+// *
+// **
+// ***
+// ****
+
+// Exercícios de escrita de código
+ 
+// questão 1)
+
+//const petUsuario = Number(prompt("Quantos bichinhos de estimação vocÊ tem?"))
+
+//if(petUsuario === 0){
+    //console.log("Que pena! Você pode adotar um pet")
+//}else{
+//}
+//const petsLista = []
+//for(let i = 0; i < petUsuario; i++){
+//const nomesPets = prompt("Qual o nome deles?")
+//petsLista.push(nomesPets)
+//console.log(petsLista)
+//}
+
