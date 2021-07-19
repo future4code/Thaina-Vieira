34b17164698deeeaaa83923f6bef8a2b1c331c24
Thainá <thainavieira660@gmail.com>
2021-07-19 16:07:06 -0300

    Não consegui resolver os exercicios por que meu note travou muito

---
+++ semana6/renderizacao-condicional/renderizacao-condicional/src/App.js
@@ -0,0 +1,8 @@
+import React from "react"
+import './App.css';
+import Etapa1 from "./components/Etapa1.js"
+
+
+class App extends React.component {
+
+}
---
+++ semana6/renderizacao-condicional/renderizacao-condicional/src/components/Etapa1.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+
+class Etapa1 extends React.Component {
+  render() {
+    return (
+      <div>
+         
+          <h1>ETAPA 1-DADOS GERAIS</h1>
+          <ol>
+              <li>Qual o seu nome?</li>
+              <input></input>
+          </ol>
+      </div>
+    );
+  }
+}
+
+export default Etapa1;
+
+
+
+
+
+
+ 
+  
