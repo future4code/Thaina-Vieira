65cc5ee4b167d1a701c81ceb389f29ea9a6a519d
Thainá <thainavieira660@gmail.com>
2021-08-09 17:35:35 -0300

    terminei todo o exercício

--- semana9/hook/insta-lab-hooks/insta-lab-hooks/src/components/SecaoComentario/SecaoComentario.js
+++ semana9/hook/insta-lab-hooks/insta-lab-hooks/src/components/SecaoComentario/SecaoComentario.js
@@ -12,11 +12,14 @@ const InputComment = styled.input `
     margin-right: 5px;
 `
 
+
+
 const SecaoComentario = (props) => {
-	const [input, setInput] = useState("")
+	const [comentario, setComentario] = useState("")
 
 
 	const onChangeComentario = (event) => {
+		setComentario(event.target.value)
 	}
 
 	return (
@@ -24,10 +27,10 @@ const SecaoComentario = (props) => {
 			<InputComment
 				className={'input-comentario'}
 				placeholder={'Comentário'}
-				value={""}
+				value={"comentario"}
 				onChange={onChangeComentario}
 			/>
-			<button onClick={() => { props.enviarComentario() }} >Enviar</button>
+			<button onClick={() => { props.enviarComentario(SecaoComentario) }} >Enviar</button>
 		</CommentContainer>
 	)
 }
